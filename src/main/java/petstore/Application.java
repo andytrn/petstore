2e21845583de3bc89c7e3b3600fac2108625f2d5
atran <atran@home.now>
2016-06-22 20:52:10 -0400

    Initial commit.

---
+++ src/main/java/petstore/Application.java
@@ -0,0 +1,21 @@
+package petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application entry.
+ * 
+ * @author atran
+ *
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Application.class, args);
+		
+	}
+	
+}

b59d183a4264f63aa3514369ffcf8f46d52e1493
atran <atran@home.now>
2016-06-25 07:12:40 -0400

    Added remove.

--- src/main/resources/database/petstore-hsqldb-dataload.sql
+++ src/main/resources/database/petstore-hsqldb-dataload.sql
@@ -1,5 +1,5 @@
-INSERT INTO pet VALUES (1, 'Max', 'http://petstore.com/photos/max.jpg', 'available');
+INSERT INTO pet VALUES (1, 'Max', 'http://petstore.com/photos/max.jpg', 'Available');
 
-INSERT INTO pet VALUES (2, 'Charlie', 'http://petstore.com/photos/chalie.jpg', 'available');
+INSERT INTO pet VALUES (2, 'Charlie', 'http://petstore.com/photos/chalie.jpg', 'Available');
 
-INSERT INTO pet VALUES (3, 'Buddy', 'http://petstore.com/photos/buddy.jpg', 'available');
+INSERT INTO pet VALUES (3, 'Buddy', 'http://petstore.com/photos/buddy.jpg', 'Pending');
